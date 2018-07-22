4139b9aff4cd5eac5d1b13b4dc3195c57adaba70
Daniel Schoch <d.schoch90@gmail.com>
2018-07-22 02:24:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,4 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
   return("I can't hear you!");
-} else if (string.toUpperCase === string) {
-  return("YES INDEED!");
-} else if (string === "I love you, Grandma.") {
-  return("I love you, too.");
-} else {
-  return("This didn't work the way it was supposed to.");
-}
-}
+} }
